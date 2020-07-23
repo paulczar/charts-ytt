a33e2c93d3ad733b219caeba405a02e2785e986d
Paul Czarkowski <username.taken@gmail.com>
2020-07-23 16:18:32 -0500

    fix configs
    Signed-off-by: Paul Czarkowski <username.taken@gmail.com>

--- ci/testing.sh
+++ ci/testing.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -eux
 # Install ytt
-PATH=$(mktemp -d)
-curl -L "https://github.com/k14s/ytt/releases/download/v0.28.0/ytt-linux-amd64" > $PATH/ytt
-chmod +x $PATH/ytt
+curl -L "https://github.com/k14s/ytt/releases/download/v0.28.0/ytt-linux-amd64" > /usr/local/bin/ytt
+chmod +x /usr/local/bin/ytt
